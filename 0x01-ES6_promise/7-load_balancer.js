5011ff905cf82d5665fdee13a7fc3e0895036835
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-03 12:57:31 +0000

    load-balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]);
-
+  return Promise
+    .race([chinaDownload, USDownload])
+    .then((res) => res);
 }
