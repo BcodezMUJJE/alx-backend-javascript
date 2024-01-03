60f48112b9aff4d60f293f35d58cbe8e89cf0f51
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-03 13:16:48 +0000

    try

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,6 +2,6 @@ export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by 0');
   }
-  return (numerator / denominator);
 
+  return numerator / denominator;
 }
