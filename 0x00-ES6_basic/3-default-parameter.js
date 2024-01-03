d382d80fa35f63708a46089a118f5e23390a70fa
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-03 10:48:32 +0000

    deflaut

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,9 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
+// const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => {
+//     return initialNumber + expansion1989 + expansion2019;
+// }
 
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
+// export default getSumOfHoods;
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
