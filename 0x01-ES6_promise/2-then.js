0976366ea827501d8cc66f1bbc8008d08e5c5613
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-03 11:30:34 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
-export default function handleResponseFromAPI(promise) {
-  const body = { status: 200, body: 'success' };
-
+function handleResponseFromAPI(promise) {
   return promise
-    .then(() => body)
-    .catch(() => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
-
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
+export default handleResponseFromAPI;
