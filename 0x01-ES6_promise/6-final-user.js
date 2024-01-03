c5536b0ceed9eb25648fc5c0ee74163bac7e3fd4
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-03 12:17:13 +0000

    final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,24 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName),
-    uploadPhoto(fileName)];
-
-  return Promise.allSettled(promises)
-    .then((values) => {
-      const res = [];
-      values.forEach((value) => {
-        if (value.status === 'fulfilled') {
-          res.push(value);
-        } else {
-          res.push({
-            status: value.status,
-            value: `Error: ${value.reason.message}`,
-          });
-        }
-      });
-      return res;
-    });
-
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
