a53042746cb12df26c05fde7f51efe0cc501d477
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-03 11:26:47 +0000

    promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,14 @@
 export default function getFullResponseFromAPI(success) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
         body: 'Success',
       });
+    } else {
+      reject(new Error(
+        'The fake API is not working currently',
+      ));
     }
-    reject(new Error('The fake API is not working currently'));
   });
-  return promise;
-
 }
