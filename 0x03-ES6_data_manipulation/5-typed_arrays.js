2e16942b54de81c991a12c567e26247b45ecb320
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-09 10:28:22 +0000

    typed_arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,16 @@
 // 5-typed_arrays.js
 
 function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
-    }
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const buffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
 
-    int8Array[position] = value;
+  int8Array[position] = value;
 
-    return new DataView(buffer);
-
+  return new DataView(buffer);
 }
 
 export default createInt8TypedArray;
