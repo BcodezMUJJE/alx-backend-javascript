873d0bbbeada34bcfb18943fe8318d9b90dbabca
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-09 10:30:59 +0000

    grade_by_city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,18 @@
 // 4-update_grade_by_city.js
 
 function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-        .filter(student => student.location === city)
-        .map(student => {
-            const gradeObject = newGrades.find(grade => grade.studentId === student.id);
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
 
-            return {
-                id: student.id,
-                firstName: student.firstName,
-                location: student.location,
-                grade: gradeObject ? gradeObject.grade : 'N/A'
-            };
-        });
-
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: gradeObject ? gradeObject.grade : 'N/A',
+      };
+    });
 }
 
 export default updateStudentGradeByCity;
