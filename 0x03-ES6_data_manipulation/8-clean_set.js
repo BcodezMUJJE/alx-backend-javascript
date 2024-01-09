477630a08edf9361dceca17ebbc4e780bae8dac8
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-09 10:09:53 +0000

    clean_set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
 // 8-clean_set.js
 
 function cleanSet(set, startString) {
-    const filteredValues = [...set].filter(value => value.startsWith(startString));
-    return filteredValues.join('-').substring(startString.length);
-
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  return filteredValues.join('-').substring(startString.length);
 }
 
 export default cleanSet;
