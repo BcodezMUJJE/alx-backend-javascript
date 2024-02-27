58735351c121c310a46ae5ec7508dce68741bbc3
BcodezMUJJE <billbrightmatege@gmail.com>
2024-02-27 19:01:46 +0000

    standard input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
-// 1-stdin.js
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
-
-readline.question('', name => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing\n');
-  readline.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
