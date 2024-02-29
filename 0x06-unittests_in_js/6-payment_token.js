5696bbefad56252f9ba8a1ffc8dc41376dc56e30
BcodezMUJJE <billbrightmatege@gmail.com>
2024-02-29 17:02:04 +0000

    payment token

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,6 @@
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
+module.exports = getPaymentTokenFromAPI;
